96a427c97ae007f115eb0f47dac12587d7b172fe
Olzhas Askar <olzhas.askar@gmail.com>
2018-06-21 20:32:34 +0200

    Added a dummy areArraysEquivalent function and its test

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-document.getElementsByTagName ("header")[0].style.backgroundColor = 'blue';
- 
+function arraysAreEquivalent(arr1, arr2) {
+  // Daniel
+
+  // write a function, which accepts two arrays
+  // and returns true if both have the same elements
+  // which do not have to be in order
+
+  // arraysAreEquivalent([2, 1], [1, 2]) should return true
+  return true;
+}
+
+// Tanya
+// write test function with various inputs to test arraysAreEquivalent
