18e5a54f66fc7a085b1f3567cb96b5867bf7def5
Olzhas Askar <olzhas.askar@gmail.com>
2018-06-21 20:46:08 +0200

    Added createLottoArray

--- js/lotto.js
+++ js/lotto.js
@@ -1,1 +1,13 @@
-document.body.style.backgroundColor = "red";
+function createLottoArray(lotto, outOf) {
+  // Kumar
+
+  // write a function, which accepts to parameters
+  // lotto - number of lotto numbers
+  // outOf - the every possible number
+  // and returns an arrays of 6 out of 49 should return random six numbers (not repeating)
+  // starting from 1 to inclusive 49
+}
+
+// Azat
+// write test which gives different numbers of lotto and tests the returning array length
+// and the numbers in the arrays
