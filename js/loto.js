5c930c8164260dc0fbc2d67f0d6fff82096105c9
Daniel <dan.streif@gmail.com>
2018-07-12 20:05:14 +0200

    Update of arraysAreEqual Function

--- js/loto.js
+++ js/loto.js
@@ -4,22 +4,19 @@
   // and returns true if both have the same elements
   // which do not have to be in order
 
-var arr1 = [1, 2, 3, 4, 5];
-var arr2 = [5, 4, 3, 2, 1];
-// Function to check if arr1 equals arr2
-var arraysAreEquivalent = function (arr1, arr2) {
-  // Sort arrays min to max
-  arr1.sort();  
-  arr2.sort();
-   // Compare length of arrays
-	 if (arr1.length !== arr2.length) return false;
-    // Compare arrays as strings if length is equal
-			if (arr1.toString() !== arr2.toString()) return false;
-  // Return answer true if no return false until here
+function arraysAreEquivalent(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  a.sort();  
+  b.sort();
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
   return true;
-};
-console.log("arr1 equals arr2", arraysAreEquivalent(arr1, arr2)); 
-
+}
 
 // Tanya
 // write test function with various inputs to test arraysAreEquivalent
