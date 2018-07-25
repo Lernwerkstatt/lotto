5b666554f461e48d4311608554857d9b58fef054
atranarta <radyuk.tatyana@gmail.com>
2018-07-25 10:12:25 +0200

    added assignin class to selected number

--- js/test.js
+++ js/test.js
@@ -1,10 +1,16 @@
 function createNewElement(number) {
   var newLabel = document.createElement("LABEL");
   var newElement = document.createElement("INPUT");
-  newElement.setAttribute("type", "checkbox");    
+  newElement.setAttribute("type", "checkbox");
   newElement.onclick = function () {
     changeSelection(number);
-  };  
+    if (this.checked) {
+      this.parentElement.classList.add("selected");
+    } else {
+      this.parentElement.classList.remove("selected");
+    }
+  };
+
   newElement.className = "box";
   newElement.id = number;
   var newSpan = document.createElement("SPAN");
@@ -13,15 +19,18 @@ function createNewElement(number) {
   newLabel.appendChild(newSpan);
   return newLabel;
 }
+
 var container = document.getElementById("container");
 for (var i = 1; i < 50; i++) {
   var element = createNewElement(i);
-  container.appendChild(element);  
+  container.appendChild(element);
 }
+
 function changeSelection(number) {
   var selection = document.getElementById("selection");
   selection.innerHTML += number + " ";
 }
+
 function checkboxes() {
   var inputElems = document.getElementsByTagName("input");
   var count = 0;
@@ -29,8 +38,8 @@ function checkboxes() {
   var selectedNumber = [];
   for (var i = 0; i < inputElems.length; i++) {
     if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
-      count++; 
-      selectedNumber.push(parseInt(inputElems[i].id));    
+      count++;
+      selectedNumber.push(parseInt(inputElems[i].id));
     }
   }
   if (count !== 6) {
@@ -40,6 +49,6 @@ function checkboxes() {
   else {
     //alert('Your numbers will be checked.');
     alert("You have " + numberOfCorrectGuesses(selectedNumber, generateArray(6, 1, 49)) + " number(s) guessed right.");
-    location.reload(); 
+    location.reload();
   }
 }
