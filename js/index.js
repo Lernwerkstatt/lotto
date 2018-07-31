97234d67a33bced47fdf88b526e3295069f236ef
atranarta <radyuk.tatyana@gmail.com>
2018-07-31 21:02:01 +0200

    Centering of the Alert Box

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ function checkboxes() {
     }
   }
   if (count !== 6) {
-    Alert.render('Please select exactly 6 numbers');
+    Alert.render("Please select exactly 6 numbers");
   }
   else {
     Alert.render("You have " + numberOfCorrectGuesses(selectedNumber, generateLottoArray(6, 1, 49)) + " number(s) guessed right.");
@@ -52,23 +52,18 @@ function checkboxes() {
 
 function CustomAlert() {
   this.render = function(dialog){
-      var winW = window.innerWidth;
-      var winH = window.innerHeight;
       var dialogoverlay = document.getElementById('dialogoverlay');
       var dialogbox = document.getElementById('dialogbox');
       dialogoverlay.style.display = "block";
-      dialogoverlay.style.height = winH+"px";
-      dialogbox.style.left = (winW/2) - (550 * .5)+"px";
-      dialogbox.style.top = "100px";
       dialogbox.style.display = "block";
       document.getElementById('dialogboxbody').innerHTML = dialog;
       document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
   }
   
-this.ok = function() {
-  document.getElementById('dialogbox').style.display = "none";
-  document.getElementById('dialogoverlay').style.display = "none";
-  location.reload();
-}
+  this.ok = function() {
+    document.getElementById('dialogbox').style.display = "none";
+    document.getElementById('dialogoverlay').style.display = "none";
+    location.reload();
+  }
 }
 var Alert = new CustomAlert();
