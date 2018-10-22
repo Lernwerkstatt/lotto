740b1b3c0a64771c2de380e30773204d16b0b693
Andreas Gropp <starmaerker@googlemail.com>
2018-10-22 17:55:48 +0200

    evaluated radio button

--- js/feedback.js
+++ js/feedback.js
@@ -1,7 +1,7 @@
 var database = firebase.database();
 var db;
 let data = {};
-var utc = new Date().toJSON().slice(0,19).replace(/T/g,'-');
+var utc = new Date().toJSON().slice(0, 19).replace(/T/g, '-');
 
 function collectData() {
 
@@ -10,17 +10,26 @@ function collectData() {
     let number = document.getElementsByName("number");
     let role = document.getElementById("dropdown");
 
+    let radio = document.getElementsByName("likelyhood");
 
+    let radio_checked;
 
-    writeUserData(utc, name[0].value, email[0].value, number[0].value, role.options[role.options.selectedIndex].innerHTML);
+    for (let i = 0; i < radio.length; i++) {
+        if (radio[i].checked === true) {
+            radio_checked = radio[i].value;
+        }
+    }
+
+    writeUserData(utc, name[0].value, email[0].value, number[0].value, role.options[role.options.selectedIndex].innerHTML, radio_checked);
 
 }
 
-function writeUserData(utc, name, email, number, role) {
+function writeUserData(utc, name, email, number, role, radio) {
     firebase.database().ref('feedback/' + utc).set({
         username: name,
         email: email,
         age: number,
-        role: role
+        role: role,
+        "Play again": radio
     });
 }
