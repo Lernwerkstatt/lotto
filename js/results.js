8e582b9154a7a79c1fab83ab40120ba7a1fa1bfb
Andreas Gropp <starmaerker@googlemail.com>
2018-10-26 06:14:57 +0200

    read keys from db

--- js/results.js
+++ js/results.js
@@ -1,1 +1,19 @@
+var database = firebase.database();
+var db;
+
+var highscoreGlobal = function retrieveFromFirebase() {
+    return database.ref().child('feedback').once('value').then(function (snapshot) {
+      db = snapshot.val();      
+    });
+  }
+
+highscoreGlobal();
+
+let age = [];
+let keys = Object.keys(db);
+
+for (let i = 0; i < keys.length; i++) {
+    age.push(db[keys[i]].age);
+}
+
 d3.select('h1').style('color', 'red');
