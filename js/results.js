f2a6e64bcbffd1bc7c59cf71b23678100ec820b4
Andreas Gropp <starmaerker@googlemail.com>
2018-10-27 18:34:42 +0200

    added random user generator

--- js/results.js
+++ js/results.js
@@ -1,28 +1,57 @@
 var database = firebase.database();
 var db;
 let age = [];
+let country = [];
+
 
 var highscoreGlobal = function retrieveFromFirebase() {
-  return database.ref().child('feedback').once('value').then(function (snapshot) {
-    db = snapshot.val();
-    let keys = Object.keys(db);
+    return database.ref().child('feedback').once('value').then(function (snapshot) {
+        db = snapshot.val();
+        let keys = Object.keys(db);
 
-    for (let i = 0; i < keys.length; i++) {
-      age.push(db[keys[i]].age);
-    }
+        for (let i = 0; i < keys.length; i++) {
+            age.push(db[keys[i]].age);
+            country.push(db[keys[i]].country);
+        }
 
-  });
+        //test(age);
+        //test(country);
+    });
 }
 
-highscoreGlobal();
+//highscoreGlobal();
 
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+function getUser() {
+    fetch("https://randomuser.me/api/?results=100")
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error("Request failed.");
+        })
+        .then(data => {
+            for (let i = 0; i < 100; i++) {
+                age.push(data.results[i].dob.age);
+            }
+            d3Test(age);
+        }
 
-d3.select("body")
-      .append("h4")
-      .data(dataset)
-      .enter()
-      .append("h4")
-      .text("Learning D3")
-      .style('color', 'red');
+        )
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+const d3Test = function (data) {
+    
+    d3.select("body")
+        .append("h4")
+        .data(data)
+        .enter()
+        .append("h4")
+        .text((d) => d)
+        .style('color', 'red');
+
+}
+getUser();
+
+
 
